825937204fe868d265393ef6ac6d74b9f44dfed3
Gianthard-cyh <2953427626@qq.com>
2022-09-29 12:51:23 +0800

    chore: init

---
+++ examples/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NodeEditor from '../pakage/nodeEditor/index.vue'
+</script>
+
+<template>
+  <NodeEditor />
+</template>
+
+<style scoped>
+
+</style>
---
+++ pakage/nodeEditor/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    nodeEditor
+  </div>
+</template>
+
+<style scoped>
+
+</style>
