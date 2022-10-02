63b9e477ad79a2dada15e469ac44aa423285979e
Gianthard-cyh <2953427626@qq.com>
2022-10-02 21:12:05 +0800

    📝update example

--- examples/App.vue
+++ examples/App.vue
@@ -3,12 +3,16 @@ import { NodeData } from '../package/common/types'
 import { EditorData } from '../package/common/types/editorData'
 import NodeEditor from '../package/nodeEditor/index.vue'
 
-const data = new EditorData({}).addNode({ title: '123' })
-const node1 = new NodeData({ title: '133' }).addPort({ title: '6636', mode: 'out' }).addPort({ title: '6636' })
-const node2 = new NodeData({ title: '133' }).addPort({ title: '6636' }).addPort({ title: '6636' })
-data.addNode(node1).addNode(node2).defineNode({ title: 'defined node', ports: [{ title: 'defined port', mode: 'out', type: { name: 'string', color: 'rgba(200,50,0)' } }] })
-  .defineNode({ title: 'defined node 2', ports: [{ title: 'defined port 2', mode: 'in' }] })
-  .defineNode({ title: 'defined node 3', ports: [{ title: 'defined port 2', mode: 'in', type: { name: 'number', color: 'rgba(200,200,20)' } }] })
+const data = new EditorData({})
+const numberType = { name: 'number', color: 'rgba(20,200,20)' }
+data.defineNode({
+  title: '相加',
+  ports: [
+    { title: 'A', type: numberType, mode: 'in' },
+    { title: 'B', type: numberType, mode: 'in' },
+    { title: '结果', type: numberType, mode: 'out' },
+  ],
+})
 </script>
 
 <template>
